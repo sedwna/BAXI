da9208eb586c50d8026a7ea605f70edfddeaa814
Navid P.Panahi <navidppanahi@gmail.com>
2024-02-17 14:58:37 +0330

    added more tables to main.sql

--- db/main.sql
+++ db/main.sql
@@ -8,21 +8,23 @@ USE baxi_staff;
 CREATE TABLE	employees
 				(
 					personnel_code			INT							PRIMARY KEY		AUTO_INCREMENT,
+					shaba_number			CHAR(26)					UNIQUE			NOT NULL,
 					password				VARCHAR(50)									NOT NULL,
 					first_name				VARCHAR(50)									NOT NULL,
 					last_name				VARCHAR(50)									NOT NULL,
-					shaba_number			CHAR(26)					UNIQUE			NOT NULL,
 					birth_date				DATE										NOT NULL,
-					proficiency				ENUM										NOT NULL,
+					salary					INT											NOT NULL,
+					department				VARCHAR(50)									NOT NULL,
+					signup_time				DATETIME									NOT NULL
+					proficiency				ENUM
 											(
 												'basic',
 												'intermediate',
 												'advanced',
 												'proficient',
 												'expert'
-											)
-					salary					INT											NOT NULL,
-					education				ENUM										NOT NULL,
+											)											NOT NULL,
+					education				ENUM
 											(
 												'none',
 												'high school diploma',
@@ -30,15 +32,14 @@ CREATE TABLE	employees
 												'bachelor''s degree',
 												'master''s degree',
 												'phd'
-											)
-					position				ENUM										NOT NULL,
+											)											NOT NULL,
+					position				ENUM
 											(
 												'department manager',
 												'basic employee',
 												'programmer'
-											)
-					department				VARCHAR(50)									NOT NULL,
-					profile_picture_path	VARCHAR(50)
+											)											NOT NULL,
+					profile_picture_path	VARCHAR(50),
 				);
 
 USE baxi_users;
@@ -47,25 +48,24 @@ CREATE TABLE	clients
 				(
 					client_id		INT				PRIMARY KEY		AUTO_INCREMENT,
 					phone_number	CHAR(11)		UNIQUE			NOT NULL,
+					wallet_balance	INT				DEFAULT 0		NOT NULL,
 					first_name		VARCHAR(50)						NOT NULL,
 					last_name		VARCHAR(50)						NOT NULL,
-					sex				ENUM							NOT NULL,
+					signup_time		DATETIME						NOT NULL
+					sex				ENUM
 									(
 										'M',
 										'F'
-									)
-					wallet_balance	INT				DEFAULT 0		NOT NULL,
-					email			VARCHAR(50)
+									)								NOT NULL,
+					email			VARCHAR(50),
 				);
 
 CREATE TABLE	transactions
 				(
 					tracking_code		INT						PRIMARY KEY,
-					amount				INT													NOT NULL,
-					source_card			CHAR(16),
-					destination_card	CHAR(16),
 					time				DATETIME				DEFAULT CURRENT_TIMESTAMP	NOT NULL,
-					state				ENUM												NOT NULL,
+					amount				INT													NOT NULL,
+					state				ENUM
 										(
 											'failed',
 											'declined',
@@ -73,35 +73,29 @@ CREATE TABLE	transactions
 											'cancelled',
 											'completed',
 											'returned'
-										)
-					type				ENUM												NOT NULL
+										)													NOT NULL,
+					type				ENUM
 										(
 											-- verification needed
 											'wallet-to-wallet',
 											'card-to-wallet',
 											'wallet-to-card',
 											'cash'
-										)
+										)													NOT NULL
+					source_card			CHAR(16),
+					destination_card	CHAR(16),
 				);
 
 CREATE TABLE	baxi_drivers
 				(
 					driver_id									INT										PRIMARY KEY		AUTO_INCREMENT,
 					driver_phone_number							CHAR(11)								UNIQUE			NOT NULL,
-					driver_first_name							VARCHAR(50)												NOT NULL,
-					driver_last_name							VARCHAR(50)												NOT NULL,
-					driver_birth_date							DATE													NOT NULL,
-					driver_national_code						CHAR(10)												NOT NULL,
-					driver_sex									ENUM													NOT NULL,
-																(
-																	'M',
-																	'F'
-																)
-					driver_wallet_balance						INT										DEFAULT 0		NOT NULL,
 					dirver_shaba_number							CHAR(26)								UNIQUE			NOT NULL,
 					driver_referral_code						CHAR(10)								UNIQUE			NOT NULL,
+					driver_wallet_balance						INT										DEFAULT 0		NOT NULL,
 					driver_disability							ENUM
 																(
+																	'none',
 																	'alzheimer''s disease',
 																	'epilepsy',
 																	'hearing loss',
@@ -111,26 +105,144 @@ CREATE TABLE	baxi_drivers
 																	'parkinson''s disease',
 																	'developmental disabilities',
 																	'physical disabilities'
-																),
-					driver_profile_picture_path					VARCHAR(50),
+																)										DEFAULT 'none'	NOT NULL,
+					driver_first_name							VARCHAR(50)												NOT NULL,
+					driver_last_name							VARCHAR(50)												NOT NULL,
+					driver_birth_date							DATE													NOT NULL,
+					driver_national_code						CHAR(10)												NOT NULL,
 					driver_license_photo_path					VARCHAR(50)												NOT NULL,
 					driver_national_card_photo_path				VARCHAR(50)												NOT NULL,
 					driver_license_verification_date			DATE													NOT NULL,
 					driver_judicial_letter_path					VARCHAR(50)												NOT NULL,
 					driver_judicial_letter_verification_date	DATE													NOT NULL,
 					driver_final_verification_date				DATE													NOT NULL,
+					driver_signup_time							DATETIME												NOT NULL,
 					vehicle_license_plate						CHAR(8)													NOT NULL,
 					vehicle_capacity							INT														NOT NULL,
 					vehicle_color								VARCHAR(50)												NOT NULL,
-					vehicle_model								VARCHAR(50)												NOT NULL,
+					vehicle_name								VARCHAR(50)												NOT NULL,
 					vehicle_production_date						DATE													NOT NULL,
-					vehicle_fuel_type							ENUM													NOT NULL,
+					vehicle_card_photo							VARCHAR(50)												NOT NULL,
+					vehicle_fuel_type							ENUM
 																(
 																	'gasoline',
 																	'CNG',
 																	'dual',
 																	'electricity'
-																)
-					vehicle_card_photo							VARCHAR(50)												NOT NULL,
+																)														NOT NULL,
+					driver_sex									ENUM
+																(
+																	'M',
+																	'F'
+																)														NOT NULL,
+					driver_profile_picture_path					VARCHAR(50),
 					verifier_personnel_code						INT														NOT NULL,
+					FOREIGN KEY(verifier_personnel_code) REFERENCES employees(personnel_code)
+				);
+
+CREATE TABLE	baxi_baar_drivers
+				(
+					driver_id									INT										PRIMARY KEY		AUTO_INCREMENT,
+					driver_phone_number							CHAR(11)								UNIQUE			NOT NULL,
+					dirver_shaba_number							CHAR(26)								UNIQUE			NOT NULL,
+					driver_referral_code						CHAR(10)								UNIQUE			NOT NULL,
+					driver_wallet_balance						INT										DEFAULT 0		NOT NULL,
+					driver_disability							ENUM
+																(
+																	'none',
+																	'alzheimer''s disease',
+																	'epilepsy',
+																	'hearing loss',
+																	'paralysis',
+																	'reduced limb or finger function',
+																	'weakened muscles',
+																	'parkinson''s disease',
+																	'developmental disabilities',
+																	'physical disabilities'
+																)										DEFAULT 'none'	NOT NULL,
+					driver_first_name							VARCHAR(50)												NOT NULL,
+					driver_last_name							VARCHAR(50)												NOT NULL,
+					driver_birth_date							DATE													NOT NULL,
+					driver_national_code						CHAR(10)												NOT NULL,
+					driver_license_photo_path					VARCHAR(50)												NOT NULL,
+					driver_national_card_photo_path				VARCHAR(50)												NOT NULL,
+					driver_license_verification_date			DATE													NOT NULL,
+					driver_judicial_letter_path					VARCHAR(50)												NOT NULL,
+					driver_judicial_letter_verification_date	DATE													NOT NULL,
+					driver_final_verification_date				DATE													NOT NULL,
+					driver_signup_time							DATETIME												NOT NULL,
+					vehicle_license_plate						CHAR(8)													NOT NULL,
+					vehicle_capacity							INT														NOT NULL,
+					vehicle_color								VARCHAR(50)												NOT NULL,
+					vehicle_name								VARCHAR(50)												NOT NULL,
+					vehicle_production_date						DATE													NOT NULL,
+					vehicle_card_photo							VARCHAR(50)												NOT NULL,
+					vehicle_fuel_type							ENUM
+																(
+																	'gasoline',
+																	'CNG',
+																	'dual',
+																	'electricity'
+																)														NOT NULL,
+					driver_sex									ENUM
+																(
+																	'M',
+																	'F'
+																)														NOT NULL,
+					driver_profile_picture_path					VARCHAR(50),
+					verifier_personnel_code						INT														NOT NULL,
+					FOREIGN KEY(verifier_personnel_code) REFERENCES employees(personnel_code)
+				);
+
+CREATE TABLE	baxi_box_drivers
+				(
+					driver_id									INT										PRIMARY KEY		AUTO_INCREMENT,
+					driver_phone_number							CHAR(11)								UNIQUE			NOT NULL,
+					dirver_shaba_number							CHAR(26)								UNIQUE			NOT NULL,
+					driver_referral_code						CHAR(10)								UNIQUE			NOT NULL,
+					driver_wallet_balance						INT										DEFAULT 0		NOT NULL,
+					driver_disability							ENUM
+																(
+																	'none',
+																	'alzheimer''s disease',
+																	'epilepsy',
+																	'hearing loss',
+																	'paralysis',
+																	'reduced limb or finger function',
+																	'weakened muscles',
+																	'parkinson''s disease',
+																	'developmental disabilities',
+																	'physical disabilities'
+																)										DEFAULT 'none'	NOT NULL,
+					driver_first_name							VARCHAR(50)												NOT NULL,
+					driver_last_name							VARCHAR(50)												NOT NULL,
+					driver_birth_date							DATE													NOT NULL,
+					driver_national_code						CHAR(10)												NOT NULL,
+					driver_license_photo_path					VARCHAR(50)												NOT NULL,
+					driver_national_card_photo_path				VARCHAR(50)												NOT NULL,
+					driver_license_verification_date			DATE													NOT NULL,
+					driver_judicial_letter_path					VARCHAR(50)												NOT NULL,
+					driver_judicial_letter_verification_date	DATE													NOT NULL,
+					driver_final_verification_date				DATE													NOT NULL,
+					driver_signup_time							DATETIME												NOT NULL,
+					vehicle_license_plate						CHAR(8)													NOT NULL,
+					vehicle_capacity							INT														NOT NULL,
+					vehicle_name								VARCHAR(50)												NOT NULL,
+					vehicle_production_date						DATE													NOT NULL,
+					vehicle_card_photo							VARCHAR(50)												NOT NULL,
+					vehicle_fuel_type							ENUM
+																(
+																	'gasoline',
+																	'CNG',
+																	'dual',
+																	'electricity'
+																)														NOT NULL,
+					driver_sex									ENUM
+																(
+																	'M',
+																	'F'
+																)														NOT NULL,
+					driver_profile_picture_path					VARCHAR(50),
+					verifier_personnel_code						INT														NOT NULL,
+					FOREIGN KEY(verifier_personnel_code) REFERENCES employees(personnel_code)
 				);
