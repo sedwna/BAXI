96734cb956beded98a4b1936c87c05c944d1b31d
Sajad Dehqan <prisoner.sedwna@gmail.com>
2024-02-19 00:10:14 +0330

    load google map in show_map

--- src/show_map.py
+++ src/show_map.py
@@ -1,52 +1,85 @@
-import io
+# import io
+# import sys
+# from PyQt6.QtWidgets import QApplication
+# from PyQt6.QtWidgets import QMainWindow
+# from PyQt6.QtWidgets import QFileDialog
+# from PyQt6.QtWidgets import QVBoxLayout
+# from PyQt6.QtWidgets import QWidget
+# from PyQt6.QtWidgets import QHBoxLayout
+#
+# from PyQt6.QtWebEngineWidgets import QWebEngineView
+# import folium
+#
+#
+# class MyApp(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.setWindowTitle("map")
+#         self.window_width, self.window_height = 360, 640
+#         self.setMinimumSize(self.window_width, self.window_height)
+#
+#         layout = QVBoxLayout()
+#         self.setLayout(layout)
+#
+#         coordinate = (34.79681370911893, 48.507035740072375)
+#         m = folium.Map(
+#
+#             title="Hamedan",
+#             zoom_start=13,
+#             Location=(34.79681370911893, 48.507035740072375)
+#         )
+#         data = io.BytesIO()
+#         m.save(data, close_file=False)
+#
+#         webView = QWebEngineView()
+#         webView.setHtml(data.getvalue().decode())
+#         layout.addWidget(webView)
+#
+#
+#
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     myApp = MyApp()
+#     myApp.show()
+#     sys.exit(app.exec())
+
+
+
+
 import sys
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWidgets import QMainWindow
-from PyQt6.QtWidgets import QFileDialog
-from PyQt6.QtWidgets import QVBoxLayout
-from PyQt6.QtWidgets import QWidget
-from PyQt6.QtWidgets import QHBoxLayout
-
+from PyQt6.QtCore import QUrl
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-import folium
 
 
-class MyApp(QWidget):
+class MapWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("map")
-        self.window_width, self.window_height = 360, 640
-        self.setMinimumSize(self.window_width, self.window_height)
 
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+        self.setWindowTitle("Google Maps Example")
+        self.setGeometry(100, 100, 800, 600)
 
-        coordinate = (34.791782835906865, 48.48797546205399)
-        m = folium.Map(
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-            title="Hamedan",
-            zoom_start=13,
-            Location=coordinate
-        )
-        data = io.BytesIO()
-        m.save(data, close_file=False)
+        layout = QVBoxLayout(central_widget)
 
-        webView = QWebEngineView()
-        webView.setHtml(data.getvalue().decode())
-        layout.addWidget(webView)
+        # Create a QWebEngineView to display Google Maps
+        self.webview = QWebEngineView()
+        layout.addWidget(self.webview)
 
+        # Load the map centered on a specific location
+        self.load_map(34.79236691747345, 48.48875557706634)
+
+    def load_map(self, lat, lon):
+        # Load the URL of Google Maps centered on the specified location
+        url = f"https://www.google.com/maps/@{lat},{lon},15.5z?entry=ttu"
+        self.webview.setUrl(QUrl(url))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyleSheet('''
-    QWidget{
-    font-size: 35px;
-    }
-    
-    
-    ''')
-    myApp = MyApp()
-    myApp.show()
-
+    window = MapWindow()
+    window.show()
     sys.exit(app.exec())
+
