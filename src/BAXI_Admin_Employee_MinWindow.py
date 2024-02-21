7d54ccab305473289967cdff12d7b8fad937eb64
Arsham2002 <arshammasoudi10@gmail.com>
2024-02-21 08:40:38 +0330

    change some text edit name Qt & comment empty commands in code and add go to query, go to recruitment

--- src/BAXI_Admin_Employee_MinWindow.py
+++ src/BAXI_Admin_Employee_MinWindow.py
@@ -23,8 +23,14 @@ class MainWindow:
         # go to sign in page from admin_panel page ----------------------------------------
         self.ui.pushButt_go_to_sign_in_admin_panel.clicked.connect(self.show_sign_in_admin)
         # ---------------------------------------------------------------------------------
+        # go to admin_panel_recruitment page from admin_panel page ----------------------------------------
+        self.ui.pushButt_add_new_employee_admin_panel.clicked.connect(self.show_admin_panel_recruitment)
+        # ---------------------------------------------------------------------------------
         # go to show_admin_panel page --------------------------------------
         self.ui.pushBut_admin_sign_in.clicked.connect(self.show_admin_panel)
+        # ------------------------------------------------------------------
+        # go to admin_panel_query page --------------------------------------
+        self.ui.pushButt_query_admin_panel.clicked.connect(self.show_admin_panel_query)
         # ------------------------------------------------------------------
         # exit from show_admin_panel page ------------------------------
         self.ui.pushButt_exit_admin_panel.clicked.connect(self.exit_app)
@@ -36,35 +42,35 @@ class MainWindow:
         self.ui.pushButt_back_admin_panel_recruitment.clicked.connect(self.show_admin_panel)
         # ----------------------------------------------------------------------------------
         # upload degree of education ----------------------------------------------
-        self.ui.pushButt_upload_education_admin_panel_recruitment.clicked.connect()
+        ###self.ui.pushButt_upload_education_admin_panel_recruitment.clicked.connect()###
         # -------------------------------------------------------------------------
         # upload Profile photo -------------------------------------------------------
-        self.ui.pushButt_upload_profilePhoto_admin_panel_recruitment.clicked.connect()
+        ###self.ui.pushButt_upload_profilePhoto_admin_panel_recruitment.clicked.connect()###
         # ----------------------------------------------------------------------------
         # go to admin_panel page ------------------------------------------------------------------
         self.ui.pushButt_go_to_admin_panel_admin_panel_query.clicked.connect(self.show_admin_panel)
         # -----------------------------------------------------------------------------------------
         # queris ----------------------------------------------------
-        self.ui.pushButt_query_1_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_2_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_3_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_4_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_5_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_6_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_7_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_8_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_9_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_10_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_11_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_12_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_13_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_14_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_15_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_16_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_17_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_18_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_19_admin_panel_query.clicked.connect()
-        self.ui.pushButt_query_20_admin_panel_query.clicked.connect()
+        ###self.ui.pushButt_query_1_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_2_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_3_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_4_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_5_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_6_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_7_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_8_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_9_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_10_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_11_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_12_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_13_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_14_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_15_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_16_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_17_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_18_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_19_admin_panel_query.clicked.connect()###
+        ###self.ui.pushButt_query_20_admin_panel_query.clicked.connect()###
         # -----------------------------------------------------------
         # go to admin_panel page -----------------------------------------------------------------------------------
         self.ui.pushButt_go_to_admin_panel_admin_panel_recruitment_successful.clicked.connect(self.show_admin_panel)
@@ -92,8 +98,13 @@ class MainWindow:
         sys.exit()
 
     def show_admin_panel_recruitment_successful(self):
-        self.ui.stackedWidget.setCurrentWidget(self.ui.pushButt_go_to_admin_panel_admin_panel_recruitment_successful)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.admin_panel_recruitment_successful)
 
+    def show_admin_panel_query(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.admin_panel_query)
+
+    def show_admin_panel_recruitment(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.admin_panel_recruitment)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
