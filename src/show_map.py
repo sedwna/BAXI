69325f41d6d3edc4a4d9a59cff981fc2c735116b
Sajad Dehqan <prisoner.sedwna@gmail.com>
2024-03-13 14:16:43 +0330

    Update show_map.py

--- src/show_map.py
+++ src/show_map.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtCore import QUrl
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6 import QtCore
 
